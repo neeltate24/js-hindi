a011f4f8969999dc768cd8f8ac616e32be3fc045
Neel <91364705+neeltate24@users.noreply.github.com>
2023-11-09 12:04:41 +0000

    datatype summary

---
+++ datatypes-summary.js
@@ -0,0 +1,38 @@
+// Primitive Data Type
+
+// types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 3456543576654356754n
+
+// Reference (Non-Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let Obj = {
+    name:"hitesh",
+    age:"22"
+}
+
+const myFunction = function() {
+    console.log("Hello World!");
+}
+
+console.log(typeof bigNumber);
+console.log(typeof outsideTemp); //null typeof object
+console.log(typeof heros);
+console.log(typeof Obj);
+console.log(typeof myFunction);
+
