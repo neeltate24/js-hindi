7bc50d1d505e119c256001e69bab82d26a325148
Neel <91364705+neeltate24@users.noreply.github.com>
2023-11-09 09:09:39 +0000

    string to number conversion confusing 7

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -27,3 +27,52 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+// ***************** Operations ****************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(-true);
+console.log(+"");
+console.log(-"");
+
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2+2 //readability
+
+let gameCounter = 100;
+// gameCounter++;
+++gameCounter; //prefix and postfix incremental operators
+
+console.log(gameCounter);
+
+//link to study
+//https://262.ecma-international.org/13.0/#sec-abstract-operations
+
+
+
+
+
+
+
+
