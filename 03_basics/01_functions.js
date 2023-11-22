32affa2fa3a2998eaf5d00d7e26771841405fb15
Neel <91364705+neeltate24@users.noreply.github.com>
2023-11-22 13:44:13 +0000

    01functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,31 @@
+function sayMyName(){
+    console.log("N");
+    console.log("E");
+    console.log("E");
+    console.log("L");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){ //parameters
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){ //parameters
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+// addTwoNumbers(3,"4") //arguments
+
+const result = addTwoNumbers(3,5)
+
+console.log("Result :", result);
+
+
+
+
+
+
+
