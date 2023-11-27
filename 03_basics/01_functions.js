d82185e685285473b94069eaace65aaf375d11c0
Neel <91364705+neeltate24@users.noreply.github.com>
2023-11-27 07:07:37 +0000

    functions_end

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -21,7 +21,7 @@ function addTwoNumbers(number1, number2){ //parameters
 
 const result = addTwoNumbers(3,5)
 
-console.log("Result :", result);
+// console.log("Result :", result);
 
 function loginUserMessage(username = "sam"){
     if(!username){  // !username = username===undefined
@@ -31,7 +31,37 @@ function loginUserMessage(username = "sam"){
 }
 
 // console.log(loginUserMessage("neel"));
-console.log(loginUserMessage("neel"));
+// console.log(loginUserMessage("neel"));
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "neel",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]  //400
+}
+
+console.log(returnSecondValue(myNewArray));
 
 
 
