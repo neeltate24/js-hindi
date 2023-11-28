58fa62eec6f7afabc0c87e6a57b14faf0dadd766
Neel <91364705+neeltate24@users.noreply.github.com>
2023-11-28 12:16:01 +0000

    one switch truthy

---
+++ 04_control_flow/one.js
@@ -0,0 +1,53 @@
+// if
+
+const isUserLoggedIn = true;
+const temperature = 41;
+
+if(temperature === 40){
+    console.log("less than 50");
+}else{
+    console.log("temperature is less than 50");
+}
+
+// console.log("Execute");
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if (score > 100){
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+
+const balance = 1000
+
+// if(balance > 500){
+//     console.log("test");
+// }
+
+if(balance < 500){
+    console.log("less than");
+} else if(balance < 750){
+    console.log("less than 750");
+}else {
+    console.log("less than 1200");
+}
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==2){
+    console.log("Allow to buy the course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
+
+
