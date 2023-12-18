c48a1b4ff1010d6ad4ff35a3833527384f36543f
Neel <91364705+neeltate24@users.noreply.github.com>
2023-12-18 11:52:54 +0000

    for in, foreach for of

---
+++ 05_iterations/five.js
@@ -0,0 +1,39 @@
+const coding = ["js","ruby","java","python","cpp"]
+
+// coding.forEach( function (val) {
+//     // console.log(val);
+// })
+
+
+coding.forEach((val)=>{
+    // console.log(val);
+})
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach((val, index, arr)=>{
+//     console.log(val, index, arr);
+// })
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js",
+    },
+    {
+        languageName: "java",
+        languageFileName: "java",
+    },
+    {
+        languageName: "python",
+        languageFileName: "py",
+    }
+]
+
+myCoding.forEach((val)=>{
+    console.log(val.languageFileName);
+})
--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -4,9 +4,9 @@
 
 //[{},{},{}]
 
-const arr = [1,2,3,4,5]
+const numbers= [1,2,3,4,5]
 
-for (const num of arr) {
+for (const num of numbers) {
     // console.log(num);
 }
 
@@ -18,9 +18,27 @@ for (const greet of greetings) {
 
 //Maps   //unique values
 
-const map = new Map()
+const map = new Map() // only prints once every property // maps for unique values only
 map.set('IN','India')
 map.set('USA','United States of America')
 map.set('FR','France')
+map.set('IN','India')
 
-console.log(map);
+// console.log(map);
+
+for (const key of map) {
+    // console.log(key);
+}
+
+for (const [key, value] of map) {
+    console.log(key, ':-', value);
+}
+
+const myObject = {
+    'game1': 'NFS',
+    'game2': 'Spiderman'
+}
+
+// for (const [key,value] of myObject) {
+//     console.log(key, ':-', value);  // this doesnt work in 
+// }
